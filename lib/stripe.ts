ecdca6961f42face1ec3ce5f7b7b1db2c49deb9a
trevormichel8-svg <trevormichel8@gmail.com>
2026-01-03 08:54:00 -0700

    Fix Stripe apiVersion typing issue (use default)

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,8 @@
 import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2023-10-16",
-})
+export const stripe = new Stripe(
+  process.env.STRIPE_SECRET_KEY!
+)
 
 export const PRICES = {
   "20": { credits: 20, amount: 699 },   // $6.99
