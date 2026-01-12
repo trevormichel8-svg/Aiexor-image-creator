5f84282735ec6b231f71d6b972fce22c6f43196b
trevormichel8-svg <trevormichel8@gmail.com>
2026-01-12 08:30:40 -0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,8 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "AI SDK Image Generator",
-  description: "An open-source AI image generator using the AI SDK by Vercel",
+  title: "AIEXOR IMAGE CREATOR",
+  description: "An open-source AI image generator using Openai",
 };
 
 export default function RootLayout({
