11511ea80adb35e8b95f68a3e4ae15dadf7c5e09
trevormichel8-svg <trevormichel8@gmail.com>
2026-01-13 06:19:56 -0700

    Fix: use default import for ImagePlayground

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { ImagePlayground } from "@/components/ImagePlayground";
+import   ImagePlayground  from "@/components/ImagePlayground";
 import { getRandomSuggestions } from "@/lib/suggestions";
 
 export const dynamic = "force-dynamic";
